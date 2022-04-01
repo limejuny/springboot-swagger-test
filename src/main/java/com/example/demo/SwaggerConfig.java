ac71c1e3f752732b6cb0f02a900bade75bcf2a9f
임준영 <limejuny@gmail.com>
2022-04-01 16:14:07 +0900

    Java 11로 버전 변경
    Detail
    ======
     - Java 8 -> 11
     - Swagger 설정 변경
     - application.properties -> application.yml

--- src/main/java/com/example/demo/SwaggerConfig.java
+++ src/main/java/com/example/demo/SwaggerConfig.java
@@ -2,6 +2,9 @@ package com.example.demo;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -10,16 +13,17 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
-public class SwaggerConfig {
+public class SwaggerConfig extends WebMvcConfigurationSupport {
+
   @Bean
   public Docket api() {
     return new Docket(DocumentationType.OAS_30)
+        .apiInfo(apiInfo())
         .useDefaultResponseMessages(false)
         .select()
         .apis(RequestHandlerSelectors.basePackage("com.example.demo.controller"))
         .paths(PathSelectors.any())
-        .build()
-        .apiInfo(apiInfo());
+        .build();
   }
 
   private ApiInfo apiInfo() {
@@ -29,4 +33,13 @@ public class SwaggerConfig {
         .version("1.0")
         .build();
   }
+
+  @Override
+  protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+    registry.addResourceHandler("/swagger-ui/**")
+        .addResourceLocations("classpath:/META-INF/resources/webjars/springfox-swagger-ui/");
+    registry.addResourceHandler("/webjars/**")
+        .addResourceLocations("classpath:/META-INF/resources/webjars/springfox-swagger-ui/");
+  }
+
 }
