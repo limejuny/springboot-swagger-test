5159731573c89522c3c49409899852f94b198f57
임준영 <limejuny@gmail.com>
2022-04-06 08:46:47 +0900

    TimeZone 변경
    Detail
    ======
     - UTC -> KST

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,10 +1,19 @@
 package com.example.demo;
 
+import java.util.TimeZone;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
+
+  @PostConstruct
+  void init() {
+    TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+  }
 
   public static void main(String[] args) {
     SpringApplication.run(DemoApplication.class, args);
